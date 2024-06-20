f8936b163840be0b3b6547c8b96c8fb86da1d90e
Cypher890 <abdulsuhaib07@gmail.com>
2024-06-20 23:33:42 +0530

    validating the token

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,9 +1,10 @@
 
 import express from 'express';
 import { createUser } from '../controllers/UserControllers';
+import { jwtCheck } from '../middleware/auth';
 
 const router = express.Router()
 
-router.post('/', createUser)
+router.post('/', jwtCheck,createUser)
 
 export default router
